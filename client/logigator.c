fe95bab8a789195a6072fa5ed74bf3a60f23a06f
Stewart Matheson <stew@rtmatheson.com>
2013-10-01 10:10:07 +1000

    logigator now watches two hard paths

--- client/logigator.c
+++ client/logigator.c
@@ -7,22 +7,51 @@
 #define EVENT_SIZE  ( sizeof (struct inotify_event) )
 #define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
 
-int main (int argc, char **argv) { 
-    int length, i = 0;
-    int fileDescriptor;
-    int watchDescriptor;
-    char buffer[BUF_LEN];
 
+typedef struct lgat_watcher {
+    char *path;
+    int watchDescriptor;
+} lgat_watcher;
+
+
+lgat_watcher create_lgat_watcher (char *path, int fileDescriptor) {
+    //first create the watch descriptor for the path
+    int watchDescriptor = inotify_add_watch( fileDescriptor, path, IN_MODIFY );
+
+    if (watchDescriptor < 0) {
+        perror( "open file" );
+        exit(1);
+    }
+
+    lgat_watcher w = { path, watchDescriptor };
+    return w;
+}
+
+void destroy_lgat_watcher (lgat_watcher w, int fileDescriptor) {
+    ( void ) inotify_rm_watch( fileDescriptor, w.watchDescriptor );
+}
+
+int main (int argc, char **argv) { 
+    int length, i = 0, j = 0, watcherCount = 2;
+    int fileDescriptor;
+    char buffer[BUF_LEN];
+    
     fileDescriptor = inotify_init();
+ 
     if (fileDescriptor < 0)  {
         perror( "open file" );
         return 1;
     }
-
-    watchDescriptor = inotify_add_watch( fileDescriptor, "/tmp/test_file", IN_MODIFY );
-    if (watchDescriptor < 0) {
-        perror( "open file" );     
-        return 1;
+ 
+    char *filesToWatch[watcherCount]; 
+    //memset( filesToWatch, 0, watcherCount * sizeof(lgat_watcher) );
+    filesToWatch[0] = "/tmp/test_file";
+    filesToWatch[1] = "/tmp/test_file2";
+    
+    lgat_watcher watchers[watcherCount];
+    
+    for (j = 0; j < watcherCount; j++) {
+        watchers[j] = create_lgat_watcher(filesToWatch[j], fileDescriptor);
     }
 
     while (1) {
@@ -35,20 +64,27 @@ int main (int argc, char **argv) {
         }
 
         while ( i <= length ) {
-            struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];    
+            // first create the struct form the buffer
+            struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];     
 
-            printf("Length: %i\n", length);
-            printf("i: %i\n", i);
-            if ( event->mask & IN_MODIFY ) {
-                printf("The file %s has been changed\n", event->name);
+            // filter only by the modify event
+            if ( event->mask & IN_MODIFY) {
+                if (i == 0) { // only fire the write event once 
+
+                    // now lets check the watchers and work out what watcher fired
+                    for (j = 0; j < watcherCount; j++) {
+                        if (watchers[j].watchDescriptor == event->wd) { 
+                            printf("%s\n", watchers[j].path);
+                        }
+                    }
+                    
+                }
             }
-            
             i += EVENT_SIZE + event->len;
         }
     
     } 
-
-    ( void ) inotify_rm_watch( fileDescriptor, watchDescriptor );
+    
     ( void ) close( fileDescriptor );
     return 0;
 } 
